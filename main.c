e4ba1d0d6bf0d3fc922a45bbcedf0bd3a0ac81cd
Admiral Potato <admiral@nuclearpixel.com>
2020-03-08 14:34:36 -0600

    Handle keyboard input by looping buttons array

--- main.c
+++ main.c
@@ -2,17 +2,28 @@
 #include <stdbool.h>
 #include <SDL.h>
 #include <SDL_image.h>
+
 #include "visual_button.h"
 
 #define SPRITE_SIZE 32
+
 #define S_0 0
 #define S_1 SPRITE_SIZE * 1
 #define S_2 SPRITE_SIZE * 2
 #define S_3 SPRITE_SIZE * 3
 
+#define BUTTON_UP    0
+#define BUTTON_DOWN  1
+#define BUTTON_LEFT  2
+#define BUTTON_RIGHT 3
+#define BUTTON_SHIFT 4
+
+#define NO_POINT { .x = 0, .y = 0 }
+
 int width = 640;
 int height = 480;
 int bitDepth = 32;
+bool loop = true;
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -44,36 +55,8 @@ bool key_left = false;
 bool key_right = false;
 bool key_shift = false;
 
-struct Button {
-    SDL_Point point;
-    bool *state;
-};
-
-#define BUTTONS 5
-struct Button buttons[] = {
-    {
-        .point = { .x = 16, .y = 0 },
-        .state = &key_up
-    },
-    {
-        .point = { .x = 16, .y = 32 },
-        .state = &key_down
-    },
-    {
-        .point = { .x = 0, .y = 16 },
-        .state = &key_left
-    },
-    {
-        .point = { .x = 32, .y = 16 },
-        .state = &key_right
-    },
-    {
-        .point = { .x = 64, .y = 16 },
-        .state = &key_shift
-    }
-};
-
 void quit (int exitCode) {
+    loop = false;
     SDL_DestroyTexture(characterTexture);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -81,6 +64,67 @@ void quit (int exitCode) {
     characterTexture = NULL;
     exit(exitCode);
 }
+
+void sdl_quit (bool state) {
+    quit(0);
+}
+
+struct Button {
+    bool *state;
+    void (*handler)(bool state);
+    bool is_renderable;
+    SDL_Scancode key;
+    SDL_Point point;
+};
+
+#define BUTTONS 6
+SDL_Scancode key;
+
+struct Button *button = NULL;
+struct Button buttons[] = {
+    {
+        .is_renderable = true,
+        .point = { .x = 32, .y = 16 },
+        .state = &key_up,
+        .key = SDL_SCANCODE_UP,
+        .handler = NULL
+    },
+    {
+        .is_renderable = true,
+        .point = { .x = 32, .y = 48 },
+        .state = &key_down,
+        .key = SDL_SCANCODE_DOWN,
+        .handler = NULL
+    },
+    {
+        .is_renderable = true,
+        .point = { .x = 16, .y = 32 },
+        .state = &key_left,
+        .key = SDL_SCANCODE_LEFT,
+        .handler = NULL
+    },
+    {
+        .is_renderable = true,
+        .point = { .x = 48, .y = 32 },
+        .state = &key_right,
+        .key = SDL_SCANCODE_RIGHT,
+        .handler = NULL
+    },
+    {
+        .is_renderable = true,
+        .point = { .x = 76, .y = 32 },
+        .state = &key_shift,
+        .key = SDL_SCANCODE_LSHIFT,
+        .handler = NULL
+    },
+    {
+        .is_renderable = false,
+        .point = NO_POINT,
+        .state = NULL,
+        .key = SDL_SCANCODE_ESCAPE,
+        .handler = sdl_quit
+    }
+};
 
 int main () {
     SDL_Init(SDL_INIT_VIDEO);
@@ -99,6 +143,7 @@ int main () {
         -1,
         SDL_RENDERER_ACCELERATED
     );
+
     initButtons(renderer);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -108,7 +153,7 @@ int main () {
     IMG_Init(IMG_INIT_PNG);
 
     characterSurface = IMG_Load("sans_sprite_uniform.png");
-    if(!characterSurface) {
+    if (!characterSurface) {
         printf("IMG_Load: %s\n", IMG_GetError());
         quit(1);
     }
@@ -134,9 +179,11 @@ int main () {
     uint8_t walk_speed = 4;
     uint8_t run_speed = 8;
     bool any_movement = false;
-    while (1) {
+
+    while (loop) {
         // Loop over all events that were pushed into the event queue since last tick
-        while(SDL_PollEvent(&event)) {
+        while(SDL_PollEvent(&event))
+        {
             if (event.type == SDL_QUIT) {
                 printf("SDL_QUIT\n");
                 quit(0);
@@ -148,27 +195,20 @@ int main () {
                 ) &&
                 !event.key.repeat
             ) {
+                key = event.key.keysym.scancode;
                 bool state = event.type == SDL_KEYDOWN;
                 // printf("SDL_KEYDOWN, %d\n", event.key.timestamp);
                 // printf("event.key.keysym.scancode, %d\n", event.key.keysym.scancode);
-                if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-                    printf("SDL_SCANCODE_ESCAPE\n");
-                    quit(0);
-                }
-                if(event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
-                    key_left = state;
-                }
-                if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
-                    key_right = state;
-                }
-                if(event.key.keysym.scancode == SDL_SCANCODE_UP) {
-                    key_up = state;
-                }
-                if(event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-                    key_down = state;
-                }
-                if(event.key.keysym.scancode == SDL_SCANCODE_LSHIFT) {
-                    key_shift = state;
+                for (int i = 0; i < BUTTONS; i++) {
+                    button = &buttons[i];
+                    if (button->key == key) {
+                        if (button->state) {
+                            *button->state = state;
+                        }
+                        if (button->handler) {
+                            button->handler(state);
+                        }
+                    }
                 }
             }
         }
@@ -206,12 +246,16 @@ int main () {
             &spriteRects[sprite_frame + sprite_direction],
             &playerRect
         );
-        for (char i = 0; i < BUTTONS; i++) {
-            drawButton(
-                renderer,
-                *buttons[i].state,
-                &buttons[i].point
-            );
+        
+        for (int i = 0; i < BUTTONS; i++) {
+            button = &buttons[i];
+            if (button->is_renderable) {
+                drawButton(
+                    renderer,
+                    *buttons[i].state,
+                    &buttons[i].point
+                );
+            }
         }
         SDL_RenderPresent(renderer);
         printf(
@@ -221,7 +265,8 @@ int main () {
             sprite_frame,
             sprite_direction
         );
-        SDL_Delay(66);
+
+        SDL_Delay(32);
     }
 
     return 0;
