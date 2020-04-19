367b2ea926fe65f300f49521644d1a956d64f6c4
Admiral Potato <admiral@nuclearpixel.com>
2020-04-19 17:42:49 -0600

    Add multiple animation support to character logic
    Use @corfidbizna's awesome new BlackMage spritesheet
    Add Blink Animation
    Add support for sprite frames to last multiple ticks

--- src/character.c
+++ src/character.c
@@ -11,23 +11,62 @@
 SDL_Surface *characterSurface;
 SDL_Texture *characterTexture;
 
-SDL_Rect spriteRects[16] = {
+SDL_Rect spriteRects[24] = {
         {.x =  S_0, .y =  S_0, .w =  S_1, .h =  S_1},
         {.x =  S_1, .y =  S_0, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_0, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_0, .w =  S_1, .h =  S_1},
         {.x =  S_0, .y =  S_1, .w =  S_1, .h =  S_1},
         {.x =  S_1, .y =  S_1, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_1, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_1, .w =  S_1, .h =  S_1},
         {.x =  S_0, .y =  S_2, .w =  S_1, .h =  S_1},
         {.x =  S_1, .y =  S_2, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_2, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_2, .w =  S_1, .h =  S_1},
         {.x =  S_0, .y =  S_3, .w =  S_1, .h =  S_1},
         {.x =  S_1, .y =  S_3, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_3, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_3, .w =  S_1, .h =  S_1},
+        {.x =  S_0, .y =  S_4, .w =  S_1, .h =  S_1},
+        {.x =  S_1, .y =  S_4, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_4, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_4, .w =  S_1, .h =  S_1},
+        {.x =  S_0, .y =  S_5, .w =  S_1, .h =  S_1},
+        {.x =  S_1, .y =  S_5, .w =  S_1, .h =  S_1},
+        {.x =  S_2, .y =  S_5, .w =  S_1, .h =  S_1},
+        {.x =  S_3, .y =  S_5, .w =  S_1, .h =  S_1},
 };
 
 SDL_Rect playerRect;
 uint16_t playerX = 32;
 uint16_t playerY = 32;
 
-uint8_t sprite_frame = 0;
-uint8_t sprite_direction = 0;
+uint8_t frame_durations[] = {
+    20,
+    2,
+    2,
+    2,
+    2,
+    2
+};
+
+struct animation {
+    uint8_t rowOffset;
+    uint8_t rowCount;
+};
+
+struct animation animations[] = {
+    {.rowOffset = CHARACTER_BLINK_ROW, .rowCount = CHARACTER_BLINK_ROWS},
+    {.rowOffset = CHARACTER_WALK_ROW, .rowCount = CHARACTER_WALK_ROWS}
+};
+
+uint8_t sprite_animation = 0;
+uint8_t frame_tick = 0;
+uint8_t row_duration = 0;
+uint8_t sprite_row = 0;
+uint8_t sprite_column = 0;
+uint8_t sprite_column_last = 0;
 uint8_t movement_speed = 0;
 uint8_t walk_speed = 2;
 uint8_t run_speed = 4;
@@ -38,7 +77,7 @@ void initCharacter (SDL_Renderer *renderer) {
     playerRect.y = gameRect.h / 2 - S_05;
     playerRect.h = CHARACTER_WIDTH;
     playerRect.w = CHARACTER_WIDTH;
-    characterSurface = IMG_Load("data/black_mage-tiny.png");
+    characterSurface = IMG_Load("data/blackmage_sheet.png");
     if (!characterSurface) {
         printf("IMG_Load: %s\n", IMG_GetError());
         quit(1);
@@ -65,27 +104,39 @@ void updateCharacter () {
         key_left ||
         key_right
     );
+    sprite_animation = any_movement ? CHARACTER_WALK_INDEX : CHARACTER_BLINK_INDEX;
     movement_speed = key_shift ? run_speed : walk_speed;
     movementX = 0;
     movementY = 0;
     if(key_left) {
         movementX = -movement_speed;
-        sprite_direction = 4;
+        sprite_column = 0;
     }
     if(key_right) {
         movementX = movement_speed;
-        sprite_direction = 6;
+        sprite_column = 3;
     }
     if(key_up) {
         movementY = -movement_speed;
-        sprite_direction = 2;
+        sprite_column = 2;
     }
     if(key_down) {
         movementY = movement_speed;
-        sprite_direction = 0;
+        sprite_column = 1;
+    }
+    if (sprite_column != sprite_column_last) {
+        sprite_row = 0;
+        frame_tick = 0;
+        sprite_column_last = sprite_column;
+    }
+    row_duration = frame_durations[animations[sprite_animation].rowOffset + sprite_row] / (key_shift ? 2 : 1);
+    if (frame_tick > row_duration) {
+        sprite_row = (sprite_row + 1) % animations[sprite_animation].rowCount;
+        frame_tick = 0;
+    } else {
+        frame_tick += 1;
     }
     if(any_movement) {
-        sprite_frame = (sprite_frame + 1) % 2;
         uint16_t nextX = (playerX + movementX + mapRect.w) % mapRect.w;
         uint16_t nextY = (playerY + movementY + mapRect.h) % mapRect.h;
         currentTile = getTileByCoordinate(nextX, nextY);
@@ -121,7 +172,15 @@ void drawCharacter (SDL_Renderer *renderer) {
     SDL_RenderCopy(
         renderer,
         characterTexture,
-        &spriteRects[sprite_frame + sprite_direction],
+        &spriteRects[
+            (
+                (animations[sprite_animation].rowOffset + sprite_row)
+                *
+                CHARACTER_DIRECTIONS
+            )
+            +
+            sprite_column
+        ],
         &playerRect
     );
     drawString(
--- src/character.h
+++ src/character.h
@@ -5,14 +5,26 @@
 
 #include "frame.h"
 
-#define SPRITE_SIZE 16
+#define SPRITE_SIZE 32
 
 #define S_0 0
 #define S_05 8
 #define S_1 SPRITE_SIZE * 1
 #define S_2 SPRITE_SIZE * 2
 #define S_3 SPRITE_SIZE * 3
+#define S_4 SPRITE_SIZE * 4
+#define S_5 SPRITE_SIZE * 5
 #define CHARACTER_WIDTH S_1
+
+#define CHARACTER_DIRECTIONS 4
+
+#define CHARACTER_BLINK_INDEX 0
+#define CHARACTER_BLINK_ROW 0
+#define CHARACTER_BLINK_ROWS 2
+
+#define CHARACTER_WALK_INDEX 1
+#define CHARACTER_WALK_ROW 2
+#define CHARACTER_WALK_ROWS 4
 
 extern SDL_Rect gameRect;
 extern SDL_Rect mapRect;
